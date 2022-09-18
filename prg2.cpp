a3153ec9959e9f11a1e4f203c6e3d39e47bf883d
Sumir Vats <sumirvats@gmail.com>
2022-09-18 10:04:13 +0530

    Completed program 2

---
+++ prg2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+class BubbleSort {
+private:
+	int* arr;
+	int size;
+public:
+	void swap(int *a, int *b) {
+		int temp = *a;
+		*a = *b;
+		*b = temp;
+	}
+
+	void display() {
+		for(int i=0; i<this->size; i++) {
+			cout << this->arr[i] << " ";
+		}
+		cout << "\n";
+	}
+
+	BubbleSort(int a[], int size) {
+		this->size = size;
+		this->arr = new int[size];
+
+		for(int i=0; i<size; i++) {
+			this->arr[i] = a[i];
+		}
+
+		for(int i=0; i<this->size-1; i++) {
+			for(int j=0; j<(this->size)-i-1; j++) {
+				if (this->arr[j] > this->arr[j + 1]) {
+					swap(&this->arr[j],&this->arr[j+1]);
+				}
+			}
+		}
+	}
+};
+
+int main() {
+	int n;
+	cout << "Enter array size: ";
+	cin >> n;
+
+	int arr[n];
+	srand(time(0));
+	for(int i=0; i<n; i++) {
+		arr[i] = rand();
+	}
+
+	BubbleSort b1(arr, n);
+	b1.display();
+	return 0;
+}
