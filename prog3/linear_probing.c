915b19467c56bea9bfdd448f4b99facb06bd22b5
Sumir Vats <sumirvats@gmail.com>
2022-09-27 21:05:19 +0530

    Program 3 completed

--- prog3/linear_probing.c
+++ prog3/linear_probing.c
@@ -1,141 +1,133 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-
-int hash_fun(int value,int cap)
+int hash_fun(int value, int cap)
 {
- return  value % cap;
+	return value % cap;
 }
 
-int linear_insert(int arr[],int value, int cap)
+int linear_insert(int arr[], int value, int cap)
 {
 
-    int i = hash_fun(value,cap);
-    while(arr[i] != -1 && arr[i]!= -99 && arr[i] != value)
-    {
-        i = (i+1)%cap;
-    }
-    if(arr[i] == value)
-    {
-     printf(" No Duplicates are allowed in the hash table ");
-     return 0;
-    }
+	int i = hash_fun(value, cap);
+	while (arr[i] != -1 && arr[i] != -99 && arr[i] != value)
+	{
+		i = (i + 1) % cap;
+	}
+	if (arr[i] == value)
+	{
+		printf(" No Duplicates are allowed in the hash table ");
+		return 0;
+	}
 
-    else
-    {
-        arr[i] = value;
-        printf("The value %d is inserted at %d key in the hash table ",value,i);
-        return 1;
-    }
-    return 0;
-
+	else
+	{
+		arr[i] = value;
+		printf("The value %d is inserted at %d key in the hash table ", value, i);
+		return 1;
+	}
+	return 0;
 }
 
-int UpdatedHashTable(int cap,int arr[])
+int UpdatedHashTable(int cap, int arr[])
 {
-   for(int i = 0; i < cap ;i++)
-   {
-    printf(" %d",arr[i]);
-   }
-return 0;
+	for (int i = 0; i < cap; i++)
+	{
+		printf(" %d", arr[i]);
+	}
+	return 0;
 }
 
-int linear_srch(int arr[],int value,int cap)
+int linear_srch(int arr[], int value, int cap)
 {
-  int key = hash_fun(value,cap);
-  int i = key;
-      while(arr[i]!= -1)
-      {
-          if (arr[i] == value )
-          {
-           printf("The value %d is found at %d key in the hash table ",value,i);
-           return 1;
-          }
+	int key = hash_fun(value, cap);
+	int i = key;
+	while (arr[i] != -1)
+	{
+		if (arr[i] == value)
+		{
+			printf("The value %d is found at %d key in the hash table ", value, i);
+			return 1;
+		}
 
-          i = (i+1)%cap;
-          if(i == key)
-          {
-            return 0;
-          }
-      }
-   return 0;
+		i = (i + 1) % cap;
+		if (i == key)
+		{
+			return 0;
+		}
+	}
+	return 0;
 }
 
-int linear_del(int arr[],int value,int cap)
+int linear_del(int arr[], int value, int cap)
 {
-  int key = hash_fun(value,cap);
-  int i = key;
-    while(arr[i]!= -1)
-      {
-          if (arr[i] == value)
-          {
-             arr[i] = -99;
-             printf("The value %d has been deleted from key %d in the hash table ",value,i);
-            return 1;
-          }
-          i = (i+1)%cap;
-          if(i == key)
-          {
-            return 0;
-          }
-
-      }
-   return 0;
+	int key = hash_fun(value, cap);
+	int i = key;
+	while (arr[i] != -1)
+	{
+		if (arr[i] == value)
+		{
+			arr[i] = -99;
+			printf("The value %d has been deleted from key %d in the hash table ", value, i);
+			return 1;
+		}
+		i = (i + 1) % cap;
+		if (i == key)
+		{
+			return 0;
+		}
+	}
+	return 0;
 }
 
 int main()
 {
-    int value,srch_flag,del_flag;
-    int cap = 50;
-    int option;
-    printf("THIS IS THE PROGRAM FOR LINEAR PROBING ");
-    int arr[50];
-    for (int i= 0 ;i <cap; i++)
-        {
-        arr[i] = -1;
-        }
-    while(1)
-    {
-       printf("\n*********************************************************************");
-       printf("\nOur current Hash table is :\n");
-       UpdatedHashTable(cap,arr);
-       printf("\n*********************************************************************");
-       printf("\nWHICH OPERATION DO YOU WANT TO PERFORM?\n 1. INSERT \n 2. SEARCH \n 3. DELETE \n 4. NONE\n");
-        scanf("%d",&option);
-         if ( option == 1)
-            {
+	int value, srch_flag, del_flag;
+	int cap = 50;
+	int option;
+	printf("THIS IS THE PROGRAM FOR LINEAR PROBING ");
+	int arr[50];
+	for (int i = 0; i < cap; i++)
+	{
+		arr[i] = -1;
+	}
+	while (1)
+	{
+		printf("\n*********************************************************************");
+		printf("\nOur current Hash table is :\n");
+		UpdatedHashTable(cap, arr);
+		printf("\n*********************************************************************");
+		printf("\nWHICH OPERATION DO YOU WANT TO PERFORM?\n 1. INSERT \n 2. SEARCH \n 3. DELETE \n 4. NONE\n");
+		scanf("%d", &option);
+		if (option == 1)
+		{
 
-             printf("Enter the value to be inserted ");
-             scanf("%d",&value);
-             linear_insert(arr,value,cap);
+			printf("Enter the value to be inserted ");
+			scanf("%d", &value);
+			linear_insert(arr, value, cap);
+		}
+		else if (option == 2)
+		{
+			printf("Enter the value to be searched ");
+			scanf("%d", &value);
+			srch_flag = linear_srch(arr, value, cap);
+			if (srch_flag == 0)
+				printf("The value %d is not found in the hash table ", value);
+		}
 
-           }
-         else if( option == 2)
-          {
-             printf("Enter the value to be searched ");
-             scanf("%d",&value);
-             srch_flag = linear_srch(arr,value,cap);
-             if(srch_flag == 0)
-                printf("The value %d is not found in the hash table ",value);
-
-          }
-
-          else if( option == 3)
-          {
-             printf("Enter the value to be deleted ");
-             scanf("%d",&value);
-             del_flag = linear_del(arr,value,cap);
-             if(del_flag == 0)
-                printf("The value %d is not found in the hash table ",value);
-          }
-          else
-          {
-           printf("*********************************************************************");
-           break;
-          }
-
-     }
-    return 0;
+		else if (option == 3)
+		{
+			printf("Enter the value to be deleted ");
+			scanf("%d", &value);
+			del_flag = linear_del(arr, value, cap);
+			if (del_flag == 0)
+				printf("The value %d is not found in the hash table ", value);
+		}
+		else
+		{
+			printf("*********************************************************************");
+			break;
+		}
+	}
+	return 0;
 }
-
-
--- prog3/separate_chaining.c
+++ prog3/separate_chaining.c
@@ -1,21 +1,21 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define size 51
 
 struct node
 {
-    int data;
-    struct node *next;
+	int data;
+	struct node *next;
 };
 
 typedef struct node NODE;
 
 struct search_help
 {
-    struct node *search_help_ptr;
-    int index;
-}sh1;
+	struct node *search_help_ptr;
+	int index;
+} sh1;
 
 typedef struct search_help SH;
 
@@ -23,202 +23,214 @@ NODE *HASH_TABLE[size];
 
 void init()
 {
-    int i;
-    for(i = 0; i < size; i++)
-        HASH_TABLE[i] = NULL;
+	int i;
+	for (i = 0; i < size; i++)
+		HASH_TABLE[i] = NULL;
 }
 
 void insert(int value)
 {
-    //create a newnode with value
-    NODE *newNode = malloc(sizeof(struct node));
-    newNode->data = value;
-    newNode->next = NULL;
+	// create a newnode with value
+	NODE *newNode = malloc(sizeof(struct node));
+	newNode->data = value;
+	newNode->next = NULL;
 
-    //calculate hash key
-    int key = value % size;
+	// calculate hash key
+	int key = value % size;
 
-    //check if chain[key] is empty
-    if(HASH_TABLE[key] == NULL)
-        HASH_TABLE[key] = newNode;
-    //collision
-    else
-    {
-        //add the node at the end of chain[key].
-        NODE *temp = HASH_TABLE[key];
-        while(temp->next!=NULL)
-        {
-            temp=temp->next;
-        }
+	// check if chain[key] is empty
+	if (HASH_TABLE[key] == NULL)
+		HASH_TABLE[key] = newNode;
+	// collision
+	else
+	{
+		// add the node at the end of chain[key].
+		NODE *temp = HASH_TABLE[key];
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
 
-        temp->next=newNode;
-    }
+		temp->next = newNode;
+	}
 }
 
 void print()
 {
-    int i;
+	int i;
 
-    for(i=0;i<size;i++)
-    {
-        NODE *temp = HASH_TABLE[i];
-        printf("HASH_TABLE[%d]-->",i);
-        while(temp)
-        {
-            printf("%d -->",temp->data);
-            temp = temp->next;
-        }
-        printf("NULL\n");
-    }
+	for (i = 0; i < size; i++)
+	{
+		NODE *temp = HASH_TABLE[i];
+		printf("HASH_TABLE[%d]-->", i);
+		while (temp)
+		{
+			printf("%d -->", temp->data);
+			temp = temp->next;
+		}
+		printf("NULL\n");
+	}
 }
 
-NODE *search_chaining(int value){
-    NODE *PTR;
-    int key = value % size;
-    if(HASH_TABLE[key]==NULL){
-     printf("%d th list is empty\n",key);
-    }
-    else
-        printf("%d th list is not empty\n",key);
-        for(PTR=HASH_TABLE[key];PTR->next!=NULL;PTR=PTR->next)
-        {
-            printf("Inside the first for loop");
-            if(PTR->data==value){
-                    printf("Inside the first if condition");
-                    printf("Element found in %d th list",key);
-                    return PTR;
-                }
-        }
-        if(PTR->data==value){
-            printf("Inside the second if condition");
-            printf("Element found in %d th list",key);
-            printf("Reached return statement");
-            return PTR;
-            }
-        else
-           {
-             printf("element not found in %d th list and hence not in the hashtable",key);
-             return NULL;
-           }
+NODE *search_chaining(int value)
+{
+	NODE *PTR;
+	int key = value % size;
+	if (HASH_TABLE[key] == NULL)
+	{
+		printf("%d th list is empty\n", key);
+	}
+	else
+		printf("%d th list is not empty\n", key);
+	for (PTR = HASH_TABLE[key]; PTR->next != NULL; PTR = PTR->next)
+	{
+		printf("Inside the first for loop");
+		if (PTR->data == value)
+		{
+			printf("Inside the first if condition");
+			printf("Element found in %d th list", key);
+			return PTR;
+		}
+	}
+	if (PTR->data == value)
+	{
+		printf("Inside the second if condition");
+		printf("Element found in %d th list", key);
+		printf("Reached return statement");
+		return PTR;
+	}
+	else
+	{
+		printf("element not found in %d th list and hence not in the hashtable", key);
+		return NULL;
+	}
 
-    /*if(PTR->next==NULL&&flag==0){
-        printf("Inside the the third if condition");
-        printf("Element no");
-        return NULL;}*/
+	/*if(PTR->next==NULL&&flag==0){
+		printf("Inside the the third if condition");
+		printf("Element no");
+		return NULL;}*/
 }
 
-NODE *search_chaining_deletion(int value, int *index_pointer){
-    NODE *PTR;
-    struct search_help
-    {
-    struct node *search_help_ptr;
-    int index;
-    }sh1;
-    typedef struct search_help SH;
-    int key = value % size;
-    if(HASH_TABLE[key]==NULL){
-     printf("%d th list is empty\n",key);
-    }
-    else
-        printf("%d th list is not empty\n",key);
-        for(PTR=HASH_TABLE[key];PTR->next!=NULL;PTR=PTR->next)
-        {
-            printf("Inside the first for loop");
-            if(PTR->data==value){
-                    printf("Inside the first if condition");
-                    printf("Element found in %d th list",key);
-                    *index_pointer=key;
-                    return PTR;
-                }
-        }
-        if(PTR->data==value){
-            printf("Inside the second if condition");
-            printf("Element found in %d th list",key);
-            printf("Reached return statement");
-            *index_pointer=key;
-            return PTR;
-            }
-        else
-           {
-             printf("element not found in %d th list and hence not in the hashtable",key);
-             *index_pointer=key;
-             return NULL;
-           }
+NODE *search_chaining_deletion(int value, int *index_pointer)
+{
+	NODE *PTR;
+	struct search_help
+	{
+		struct node *search_help_ptr;
+		int index;
+	} sh1;
+	typedef struct search_help SH;
+	int key = value % size;
+	if (HASH_TABLE[key] == NULL)
+	{
+		printf("%d th list is empty\n", key);
+	}
+	else
+		printf("%d th list is not empty\n", key);
+	for (PTR = HASH_TABLE[key]; PTR->next != NULL; PTR = PTR->next)
+	{
+		printf("Inside the first for loop");
+		if (PTR->data == value)
+		{
+			printf("Inside the first if condition");
+			printf("Element found in %d th list", key);
+			*index_pointer = key;
+			return PTR;
+		}
+	}
+	if (PTR->data == value)
+	{
+		printf("Inside the second if condition");
+		printf("Element found in %d th list", key);
+		printf("Reached return statement");
+		*index_pointer = key;
+		return PTR;
+	}
+	else
+	{
+		printf("element not found in %d th list and hence not in the hashtable", key);
+		*index_pointer = key;
+		return NULL;
+	}
 
-    /*if(PTR->next==NULL&&flag==0){
-        printf("Inside the the third if condition");
-        printf("Element no");
-        return NULL;}*/
+	/*if(PTR->next==NULL&&flag==0){
+		printf("Inside the the third if condition");
+		printf("Element no");
+		return NULL;}*/
 }
 
 int delete_chaining(int value)
 {
 
-    NODE *LOC, *TEMP;
-    int index;
-    /*struct delete_help
-    {
-    struct node *delete_help_ptr;
-    int index_delete;
-    }dh1;*/
-    //printf("Line 130\n");
-    //printf("Line 132\n");
-    LOC=search_chaining_deletion(value,&index);
-    if(LOC==NULL){
-        printf("Element does not lie in the hashtable");
-    }
-    if(LOC==HASH_TABLE[index]){
-        if(HASH_TABLE[index]->next==NULL)
-            HASH_TABLE[index]=NULL;
-        else
-        HASH_TABLE[index]=HASH_TABLE[index]->next;
-        return(value);
-    }
-    for(TEMP=HASH_TABLE[index];TEMP->next!=LOC;TEMP=TEMP->next)
-    ;
-    TEMP->next=LOC->next;
-    /*if(LOC->next==NULL)
-    LOC=TEMP;*/
-    return(LOC->data);
+	NODE *LOC, *TEMP;
+	int index;
+	/*struct delete_help
+	{
+	struct node *delete_help_ptr;
+	int index_delete;
+	}dh1;*/
+	// printf("Line 130\n");
+	// printf("Line 132\n");
+	LOC = search_chaining_deletion(value, &index);
+	if (LOC == NULL)
+	{
+		printf("Element does not lie in the hashtable");
+	}
+	if (LOC == HASH_TABLE[index])
+	{
+		if (HASH_TABLE[index]->next == NULL)
+			HASH_TABLE[index] = NULL;
+		else
+			HASH_TABLE[index] = HASH_TABLE[index]->next;
+		return (value);
+	}
+	for (TEMP = HASH_TABLE[index]; TEMP->next != LOC; TEMP = TEMP->next)
+		;
+	TEMP->next = LOC->next;
+	/*if(LOC->next==NULL)
+	LOC=TEMP;*/
+	return (LOC->data);
 }
 
 int main()
 {
-    //init array of list to NULL
-    int upper=10000, lower=0,i=0, new_element, search_element, delete_element, deleted_element;
+	// init array of list to NULL
+	int upper = 10000, lower = 0, i = 0, new_element, search_element, delete_element, deleted_element;
 
-    init();
-    srand(time(0));
-    while(i<size){
-        int temp, value;
-        temp=rand()%upper;
-        if(temp>=lower){
-            value=temp;
-            insert(value);
-            i++;
-        }
-        else
-            continue;
-    }
+	init();
+	srand(time(0));
+	while (i < size)
+	{
+		int temp, value;
+		temp = rand() % upper;
+		if (temp >= lower)
+		{
+			value = temp;
+			insert(value);
+			i++;
+		}
+		else
+			continue;
+	}
 
-    print();
-    
-    printf("Enter the value of the element you want to insert");
-    scanf("%d",&new_element);
-    insert(new_element);
+	print();
 
-    print();
+	printf("Enter the value of the element you want to insert");
+	scanf("%d", &new_element);
+	insert(new_element);
 
-    printf("Enter the element you want to search");
-    scanf("%d",&search_element);
-    search_chaining(search_element);
+	print();
 
-    // printf("The value at the found address is %d",search(search_element)->data);
-    printf("Enter the element you want to delete");
-    scanf("%d",&delete_element);
-    deleted_element=delete_chaining(delete_element);
+	printf("Enter the element you want to search");
+	scanf("%d", &search_element);
+	search_chaining(search_element);
 
-    print();
-    
-    return 0;
+	// printf("The value at the found address is %d",search(search_element)->data);
+	printf("Enter the element you want to delete");
+	scanf("%d", &delete_element);
+	deleted_element = delete_chaining(delete_element);
+
+	print();
+
+	return 0;
 }
